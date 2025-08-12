dd2917f95ef90f87400b95f11830d16a92672891
あっくん ゲーマー <o.akkun2005@gmail.com>
2025-08-12 19:03:51 +0900

    平行移動する問題は解決。あとは移動速度が速いとすり抜けてしまう現象の対策とZ-方向の壁だけプレイヤーと近い状態が問題

--- project/Engine/Math/AABB.h
+++ project/Engine/Math/AABB.h
@@ -7,3 +7,34 @@ struct AABB final
 	Vector3 min;
 	Vector3 max;
 };
+
+inline AABB operator+=(AABB& aabb, const float& value) {
+	aabb.min += value;
+	aabb.max += value;
+	return aabb;
+}
+
+inline AABB operator+=(AABB& aabb1, const AABB& aabb2) {
+	aabb1.min += aabb2.min;
+	aabb1.max += aabb2.max;
+	return aabb1;
+}
+
+inline AABB operator+=(AABB& aabb, const Vector3& value) {
+	aabb.min += value;
+	aabb.max += value;
+	return aabb;
+}
+
+inline AABB AddSize(AABB& aabb, const float& value) {
+	aabb.min -= value;
+	aabb.max += value;
+	return aabb;
+}
+
+inline AABB operator+(const AABB& aabb, const Vector3 value) {
+	AABB result;
+	result.min += aabb.min + value;
+	result.max += aabb.max + value;
+	return result;
+}
