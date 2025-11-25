b2f5a0a1ff2eb586e3a17ffbe00cb1c97bdf417e
Nino <o.akkun2005@gmail.com>
2025-11-25 19:00:19 +0900

    ParticleManager、Texcoordの修正

--- project/Resources/shaders/Particle/Particle.PS.hlsl
+++ project/Resources/shaders/Particle/Particle.PS.hlsl
@@ -22,7 +22,7 @@ PixelShaderOutput main(VertexShaderOutput input){
     float4 transformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     output.color = gMaterial.color * textureColor * input.color;
-    if (output.color.a < 0.2f)
+    if (output.color.a <= 0.1f)
     {
         discard;
     }
