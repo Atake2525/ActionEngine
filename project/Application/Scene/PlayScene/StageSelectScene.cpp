4f083da05378135e6f5495b9050bcd8f39299063
Nino <o.akkun2005@gmail.com>
2025-11-30 18:50:48 +0900

    ステージセレクトシーンを作成

---
+++ project/Application/Scene/PlayScene/StageSelectScene.cpp
@@ -0,0 +1,26 @@
+#include "StageSelectScene.h"
+#include "TextureManager.h"
+
+using namespace std;
+
+void StageSelectScene::Initialize() {
+	camera = make_unique<Camera>();
+	
+	TextureManager::GetInstance()->LoadTexture("Resources/rostock_laage_airport_4k.dds");
+
+}
+
+void StageSelectScene::Finalize()
+{
+
+}
+
+void StageSelectScene::Update()
+{
+
+}
+
+void StageSelectScene::Draw()
+{
+
+}
