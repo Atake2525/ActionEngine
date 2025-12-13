cba62b1e0334bd9783afcbff52520c2d69a66ddd
Atake <146906117+Atake2525@users.noreply.github.com>
2025-12-13 17:03:32 +0900

    ステージを変えた

--- project/Application/Scene/PlayScene/StageSelectScene.cpp
+++ project/Application/Scene/PlayScene/StageSelectScene.cpp
@@ -8,6 +8,9 @@ void StageSelectScene::Initialize() {
 	
 	TextureManager::GetInstance()->LoadTexture("Resources/rostock_laage_airport_4k.dds");
 
+	SkyBox::GetInstance()->SetCamera(camera.get());
+	SkyBox::GetInstance()->SetTexture("Resources/rostock_laage_airport_4k.dds");
+
 }
 
 void StageSelectScene::Finalize()
