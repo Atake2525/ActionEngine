3e53e81d0040bf01c730bc40f1cd6f6985b26c97
Atake <146906117+Atake2525@users.noreply.github.com>
2026-01-25 16:26:53 +0900

    最適化した入力処理をPlayerで使用

--- project/Engine/Math/Vector2.h
+++ project/Engine/Math/Vector2.h
@@ -36,4 +36,8 @@ inline Vector2 operator-(Vector2 v1, Vector2 v2) {
 	return v1;
 }
 
+inline Vector2 operator*(const Vector2& v, float f) {
+	return { v.x * f, v.y * f };
+}
+
 inline const Vector2 Vector2::Zero = Vector2{ 0.0f, 0.0f };
