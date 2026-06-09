97ae607d90c28e39974f92b7894a606181d03543
Atake2525 <k023g0094@g.neec.ac.jp>
2026-06-09 19:25:39 +0900

    ActionNameToEnumをunodered_mapからarrayに変更

--- project/Application/Setting/SettingManager.cpp
+++ project/Application/Setting/SettingManager.cpp
@@ -3,11 +3,10 @@
 #include <fstream>
 #include <sstream>
 #include "Logger.h"
-#include "InputConverter.h"
+
 
 using namespace Setting;
 using namespace Logger;
-using namespace InputConverter;
 
 bool SettingManager::Load(const std::string filename) {
 	
@@ -36,7 +35,7 @@ bool SettingManager::Load(const std::string filename) {
 
 	if (type == "KEYCONFIG")
 	{
-		LoadKeyConfig(deserialized);
+		m_keyBind = KeyConfig::Load(deserialized);
 	}
 	else
 	{
@@ -49,127 +48,7 @@ bool SettingManager::Load(const std::string filename) {
 }
 
 bool SettingManager::Save(const std::string filename) {
-	nlohmann::json json = JsonLoader::GetInstance()->LoadJson("Settings/" + filename);
-
-	if (!json.is_object())
-	{
-		json = nlohmann::json::object();
-	}
-
-	json["type"] = "KEYCONFIG";
-	json["main"] = nlohmann::json::object();
-
-	// 各 Action を main に書き込む
-	// keyboard は ConvertDIKToKey()
-	// gamepad は Controller / DPad / StickDirection のどれかを文字列へ戻す
-	// mouse は現状ロードで使っていないので null
-	for (const auto& action : Setting::ActionNameToEnum)
-	{
-		nlohmann::json bind;
-		bind["keyboard"] = ConvertDIKToKey(
-			static_cast<BYTE>(m_keyboardConfig.GetMainAction(action.second))
-		);
-		bind["mouse"] = nullptr;
-
-		std::string gamepad = "null";
-
-		StickDirection stick = m_controllerConfig.GetStickAction(action.second);
-		DPad dpad = m_controllerConfig.GetDPadAction(action.second);
-		Controller controller = m_controllerConfig.GetControllerAction(action.second);
-
-		if (stick != StickDirection::None) {
-			gamepad = ConvertStickDirectionToKey(stick);
-		}
-		else if (dpad != DPad::None) {
-			gamepad = ConvertDPadToKey(dpad);
-		}
-		else if (controller != Controller::None) {
-			gamepad = ConvertControllerToKey(controller);
-		}
-
-		bind["gamepad"] = gamepad == "null" ? nlohmann::json(nullptr) : nlohmann::json(gamepad);
-		json["main"][action.first] = bind;
-	}
-
-	std::ofstream file("Settings/" + filename);
-	if (!file.is_open())
-	{
-		return false;
-	}
-
-	file << json.dump(4);
-	return true;
+    bool result = false;
+    result = KeyConfig::Save(filename, m_keyBind);
+	return result;
 }
-
-void SettingManager::LoadKeyConfig(nlohmann::json json) {
-	// mainキーコンフィグの確認
-	// mainキーがnullだったらdefaultのキーコンフィグを使用する
-    std::string keyConfigKey = "main";
-	if (!json.contains(keyConfigKey) || json[keyConfigKey].is_null()) {
-		Log("mainキーコンフィグがnullのため、defaultのキーコンフィグを使用します\n");
-        keyConfigKey = "default";
-	}
-
-	if (!json.contains(keyConfigKey) || !json[keyConfigKey].is_object())
-	{
-		Log("キーコンフィグがJsonオブジェクト型ではありません。\n");
-		return;
-	}
-
-	const nlohmann::json& keyConfig = json[keyConfigKey];
-
-	for (auto it = keyConfig.begin(); it != keyConfig.end(); ++it)
-	{
-		if (it.key() == "sensitivity") // 感度設定
-		{
-			m_sensitivity.mouse = it.value()["mouse"].get<float>();
-			m_sensitivity.controller = it.value()["controller"].get<float>();
-			m_sensitivity.invertX = it.value()["invertX"].get<bool>();
-			m_sensitivity.invertY = it.value()["invertY"].get<bool>();
-			continue;
-		}
-        // キーコンフィグのキーがActionに変換できない場合はスキップする
-		std::optional<Action> action = ToAction(it.key());
-		if (!action.has_value())
-		{
-			continue;
-		}
-
-        // キーコンフィグの値がオブジェクト型でない場合はスキップする
-		const nlohmann::json& bind = it.value();
-		if (!bind.is_object())
-		{
-			continue;
-		}
-
-        // キーボードのキー設定
-		if (bind.contains("keyboard") && bind["keyboard"].is_string())
-		{
-			m_keyboardConfig.SetMainAction(action.value(), ConvertKeyToDIK(bind["keyboard"].get<std::string>()));
-		}
-
-		if (bind.contains("gamepad") && bind["gamepad"].is_string())
-		{
-			const std::string gamepadKey = bind["gamepad"].get<std::string>();
-
-			Controller controller = ConvertKeyToController(gamepadKey);
-			if (controller != Controller::None)
-			{
-				m_controllerConfig.SetAction(action.value(), controller);
-			}
-
-			DPad dpad = ConvertKeyToDPad(gamepadKey);
-			if (dpad != DPad::None)
-			{
-				m_controllerConfig.SetAction(action.value(), dpad);
-			}
-
-			StickDirection stickDirection = ConvertKeyToStickDirection(gamepadKey);
-			if (stickDirection != StickDirection::None)
-			{
-				m_controllerConfig.SetAction(action.value(), stickDirection);
-			}
-		}
-	}
-}
-
--- project/Application/Setting/SettingManager.h
+++ project/Application/Setting/SettingManager.h
@@ -1,10 +1,8 @@
 #include <string>
-#include "KeyboardConfig.h"
-#include "ControllerConfig.h"
-#include "json.hpp"
 #include <unordered_map>
 #include <optional>
 #include "SettingUtility.h"
+#include "KeyConfig.h"
 #pragma once
 
 class SettingManager
@@ -17,16 +15,11 @@ public:
     bool Save(const std::string filename);
 
     // キーコンフィグとコントローラーコンフィグを取得する
-    Setting::KeyboardConfig& GetKeyConfig() { return m_keyboardConfig; }
-    Setting::ControllerConfig& GetControllerConfig() { return m_controllerConfig; }
-    Setting::Sensitivity& GetSensitivity() { return m_sensitivity; }
+    Setting::KeyboardConfig& GetKeyConfig() { return m_keyBind.keyboardConfig; }
+    Setting::ControllerConfig& GetControllerConfig() { return m_keyBind.controllerConfig; }
+    Setting::Sensitivity& GetSensitivity() { return m_keyBind.sensitivity; }
 
 private:
-
-    void LoadKeyConfig(nlohmann::json json);
-
-    Setting::KeyboardConfig m_keyboardConfig;
-    Setting::ControllerConfig m_controllerConfig;
-    Setting::Sensitivity m_sensitivity;
+    Setting::KeyBind m_keyBind;
 };
 
