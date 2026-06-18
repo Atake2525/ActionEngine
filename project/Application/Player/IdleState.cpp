0d66d62f5841b75892a87c27434aeab291d2d802
Atake <146906117+Atake2525@users.noreply.github.com>
2026-06-18 13:02:50 +0900

    statePattern実装中

---
+++ project/Application/Player/IdleState.cpp
@@ -0,0 +1,12 @@
+#include "IdleState.h"
+#include "Player.h"
+
+void IdleState::Enter(Player* player) {
+
+}
+
+void IdleState::Update(Player* player) {
+}
+
+void IdleState::Exit(Player* player) {
+}
